b85638cdd7a30f8292a9664d1dc262995338eccd
Markus Bulheller <markus.bulheller@gmail.com>
2023-01-20 01:55:12 +0100

    styled reset, install fonts, global style init

---
+++ src/globalStyles.ts
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset'
+ 
+const GlobalStyle = createGlobalStyle`
+${reset}
+
+@font-face {
+    font-family: "Antonio Medium";
+    src: url("/fonts/Antonio-Medium.ttf");
+  }
+  @font-face {
+    font-family: "Spartan";
+    src: url("/fonts/LeagueSpartan-Regular.ttf");
+  }
+  @font-face {
+    font-family: "Spartan Bold";
+    src: url("/fonts/LeagueSpartan-Bold.ttf");
+  }
+
+  body {
+    background-color: hsl(240deg 67% 8%);
+    height: 100vh;
+    color: hsl(0deg 0% 100%);
+    font-family: 'Spartan';
+    font-size: 1rem;
+    line-height: 1.5rem;
+  }
+`;
+ 
+export default GlobalStyle;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import styled from 'styled-components'
 import App from './App'
+import GlobalStyle from './globalStyles'
+
+const Container = styled.div`
+  background: url('/images/background-stars.svg');
+  background-position: center;
+  background-repeat: no-repeat;
+  height: 100vh;
+`
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <GlobalStyle />
+    <Container>
+      <App />
+    </Container>
   </React.StrictMode>,
 )
